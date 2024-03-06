a0a7011f52d1c6255ae7b9a330381b8003255e7f
Amirali <madaniamirali7@gmail.com>
2024-03-05 19:24:14 -0500

    euler tour done, needs to be tested.

--- Tree.cpp
+++ Tree.cpp
@@ -3,13 +3,22 @@
 //
 
 #include "Tree.h"
-
-void Tree::eulerTour(Node* u) {
+int Tree::eulerTourIndexCounter=0;
+void Tree::eulerTour(Node* u, int depth) {
     std::cout<< u->id<<" ";
+    if(eulerTourNodeIDS[u->id]==-1){
+        eulerTourNodeIDS[u->id]=eulerTourIndexCounter;
+    }
+    eulerTourSequenceIDs[eulerTourIndexCounter]=u->id;
+    eulerTourSequenceDepths[eulerTourIndexCounter]=depth;
+    eulerTourIndexCounter++;
     if (u->children.size() !=0){
         for (auto const& v : u->children) {
-            eulerTour(v);
+            eulerTour(v, depth+1);
             std::cout<<u->id<<" ";
+            eulerTourSequenceIDs[eulerTourIndexCounter]=u->id;
+            eulerTourSequenceDepths[eulerTourIndexCounter]=depth;
+            eulerTourIndexCounter++;
         }
     }
 }
@@ -17,6 +26,10 @@ void Tree::eulerTour(Node* u) {
 Tree::Tree(Node * root, int n) {
     this-> root=root;
     this->numNodes=n;
+    eulerTourSequenceIDs=new int[3*numNodes-2];
+    eulerTourSequenceDepths=new int[3*numNodes-2];
+    eulerTourNodeIDS=new int[numNodes];
+    std::fill_n(eulerTourNodeIDS, numNodes, -1);
 }
 
 void Tree::addChild(Node* child){
@@ -24,5 +37,5 @@ void Tree::addChild(Node* child){
 }
 
 void Tree::eulerTour() {
-    eulerTour(root);
+    eulerTour(root,0);
 }
--- Tree.h
+++ Tree.h
@@ -10,13 +10,19 @@
 
 class Tree {
 public:
-    Node * root;
-    int * walkindex;
+    Node* root;
+    int* eulerTourSequenceIDs; /*An array of size 3n-2 containing the eulertour sequence of node ids.
+                                *eulerTourSequenceIDs[i] contains the id of the i-th node in the euler tour*/
+    int* eulerTourSequenceDepths; /*An array of size 3n-2 containing the eulertour sequence of node depths.
+                                *eulerTourSequenceDepths[i] contains the depth of the i-th node in the euler tour*/
+    int* eulerTourNodeIDS; /*An array of size n containing the eulertour sequence of node tour indexes.
+                                *eulerTourNodeIDS[i] contains the tour index of the i-th index (the first time it is visited in the tour. */
     int numNodes;
-    std::vector<Node *> children;
-    explicit Tree(Node *, int);
-    void addChild(Node *);
-    void eulerTour(Node *);
+    std::vector<Node*> children;
+    static int eulerTourIndexCounter;
+    explicit Tree(Node*, int);
+    void addChild(Node*);
+    void eulerTour(Node*, int);
     void eulerTour();
 };
 
